d5fb34c568c72e83aa56ddafff1508a128c935d5
martinapovk <martinacskapok@gmail.com>
2023-06-25 15:21:34 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import LoadMoreBtn from './js/load-more-btn';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const BASE_URL = 'https://pixabay.com/api/';
-const API_KEY = '37811543-b06f280f3d7e7be97e6295a75';
-
 const galleryRef = document.querySelector('.gallery');
 const formRef = document.querySelector('.search-form');
 const loadMoreBtnRef = document.querySelector('.load-more');
@@ -36,66 +33,6 @@ async function onLoadMore() {
   }
 }
 
-// async function loadMoreHendle() {
-//   page += 1;
-//   showLoadMore(false);
-//   try {
-//     const { totalHits, hits } = await picsApi.fetchPics();
-//     if (100 * picsApi.page > totalHits) {
-//       return Notiflix.Notify.failure(
-//         "We're sorry, but you've reached the end of search results."
-//       );
-//     }
-//     showLoadMore(true);
-//     rendermarkup(hits);
-//     autoScroll(2);
-//   } catch (error) {
-//     Notiflix.Notify.failure('Oops something gone wrong..');
-//     console.log(error);
-//   }
-// }
-
-// function loadMoreHendle() {
-//   page += 1;
-//   showLoadMore(false);
-//   makeGallery();
-//   getHitsArr();
-// .then(res => {
-//   if (!res.data.hits.length) {
-//     throw new Error(
-//       '"Sorry, there are no images matching your search query. Please try again."'
-//     );
-//   }
-//   Notiflix.Notify.success(`Hooray! We found ${res.data.totalHits} images.`);
-//   //   console.log(res.data);
-//   makeGallery(res.data.hits);
-//   showLoadMore(true);
-// })
-// .catch(err => {
-//   galleryRef.innerHTML = '';
-//   Notiflix.Report.failure('Error', `${err}`, 'OK');
-// });
-// }
-
-// async function getHitsArr() {
-//   try {
-//     const get = await axios.get(BASE_URL, {
-//       params: {
-//         key: API_KEY,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: true,
-//         q: 'cat',
-//         page: page,
-//         per_page: 3,
-//       },
-//     });
-//     // const arr = await
-//     console.log(get);
-//   } catch {
-//     console.log('errr');
-//   }
-// }
 async function submitSearchFormHendle(e) {
   e.preventDefault();
 
@@ -134,46 +71,6 @@ async function submitSearchFormHendle(e) {
     // console.log(error);
   }
 }
-
-// function submitSearchFormHendle(e) {
-//   e.preventDefault();
-//   galleryRef.innerHTML = '';
-
-//   showLoadMore(false);
-//   const searchWord = e.currentTarget.elements.searchQuery.value.trim();
-//   getData(searchWord);
-//   formRef.reset();
-// }
-
-// function getData(searchWord) {
-//   axios
-//     .get(BASE_URL, {
-//       params: {
-//         key: API_KEY,
-//         image_type: 'photo',
-//         orientation: 'horizontal',
-//         safesearch: true,
-//         q: `${searchWord}`,
-//         page: page,
-//         per_page: 3,
-//       },
-//     })
-//     .then(res => {
-//       if (!res.data.hits.length) {
-//         throw new Error(
-//           '"Sorry, there are no images matching your search query. Please try again."'
-//         );
-//       }
-//       Notiflix.Notify.success(`Hooray! We found ${res.data.totalHits} images.`);
-//       //   console.log(res.data);
-//       makeGallery(res.data.hits);
-//       showLoadMore(true);
-//     })
-//     .catch(err => {
-//       galleryRef.innerHTML = '';
-//       Notiflix.Report.failure('Error', `${err}`, 'OK');
-//     });
-// }
 
 function renderMarkup(data) {
   let markup = data.map(item => makeGalleryItemMarkup(item)).join('');
@@ -220,18 +117,3 @@ function makeGalleryItemMarkup({
 function resetMarkup() {
   galleryRef.innerHTML = '';
 }
-
-// function makeGallery(arrGalleryItems) {
-//   let markup = arrGalleryItems
-//     .map(item => makeGalleryItemMarkup(item))
-//     .join('');
-//   galleryRef.insertAdjacentHTML('beforeend', markup);
-// }
-
-// function resetMarkup() {
-//   galleryRef.innerHTML = '';
-// }
-
-// function showLoadMore(isShow) {
-//   loadMoreBtnRef.style.display = isShow ? 'block' : 'none ';
-// }
